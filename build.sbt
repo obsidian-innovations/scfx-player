0820406096876e119e922927e5c8ca806548520e
ssemakin <ssemakin@gmail.com>
2013-09-24 17:13:45 +0300

    Adding simple player controls panel. Updated build file - java_home retrieved via scala util.

--- build.sbt
+++ build.sbt
@@ -10,18 +10,20 @@ assemblySettings
 
 libraryDependencies ++= Seq(
                            "org.specs2" %% "specs2" % "2.2.2" % "test",
-                            "org" % "jaudiotagger" % "2.0.3",
+//to be removed
+//                            "org" % "jaudiotagger" % "2.0.3",
 // current stuff
                              "org.scalafx" % "scalafx_2.10" % "1.0.0-M5"
 // new stuff
 //                            "org.scalafx" % "scalafx_2.10" % "8.0.0-M1"
                             )
 
+//to be removed
 //unmanagedJars in Compile += Attributed.blank(file(System.getenv("JAVA_HOME") + "/jre/lib/jfxrt.jar"))
 unmanagedJars in Compile += Attributed.blank(file(scala.util.Properties.javaHome) / "lib" / "jfxrt.jar")
 
 
-mainClass in (Compile, run) := Some("org.scfxplayer.ScFxMediaTagTest")
+mainClass in (Compile, run) := Some("org.scfxplayer.Main")
 
 mainClass in assembly := Some("org.scfxplayer.Main")
 
--- src/main/scala/org/scfxplayer/Main.scala
+++ src/main/scala/org/scfxplayer/Main.scala
@@ -1,19 +1,19 @@
 package org.scfxplayer
 
+import java.io.File
+import javafx.event.EventHandler
+import javafx.scene.input.MouseEvent
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.scene.Scene
-import scalafx.scene.control.{TableView, Button}
-import javafx.event.EventHandler
-import javafx.scene.input.MouseEvent
 import scalafx.stage.FileChooser
-import java.io.File
-import scala.collection.JavaConversions._
 import scalafx.beans.property.StringProperty
 import scalafx.collections.ObservableBuffer
 import scalafx.scene.control.TableColumn._
-import scalafx.scene.control.{TableColumn, TableView}
-import scalafx.scene.layout.{Priority, VBox}
+import scalafx.scene.control.{TableColumn, TableView, Button}
+import scalafx.scene.layout.{HBox, Priority, VBox}
+import scalafx.geometry.Pos
+import scala.collection.JavaConversions._
 
 class MusicRecordItem(fileName_ : String, fileExt_ : String) {
   val fileName = new StringProperty(this, "fileName", fileName_)
@@ -44,27 +44,45 @@ object Main extends JFXApp {
     )
   }
 
+  val deleteFilesBtn = new Button {
+    text = "Delete"
+    prefHeight = 40
+    vgrow = Priority.NEVER
+  }
+
+  val openFilesBtn = new Button {
+    text = "Open"
+    vgrow = Priority.ALWAYS // Doesn't work?!?
+    managed = true
+    prefHeight = 40 // vgrow policy didn't work, set it directly for now
+    onMouseClicked = new EventHandler[MouseEvent] {
+      override def handle(event:MouseEvent) {
+        event.consume()
+        fchooser.showOpenMultipleDialog(parent.value.getScene.getWindow) match {
+          case null => Unit
+          case fs => {
+            musicRecItems.clear
+            musicRecItems.addAll(fs.map(f => new MusicRecordItem(f.getName, "mp3")))
+          }
+        }
+      }
+    }
+  }
+
+  val playerControlsLayout = new HBox {
+    hgrow = Priority.ALWAYS
+    alignment = Pos.CENTER_LEFT
+    minHeight = 40
+    maxHeight = 40
+    prefHeight = 40
+    fillHeight = true // Doesn't work?!?
+    content = Seq(openFilesBtn, deleteFilesBtn)
+  }
+
   val mainLayout = new VBox {
     vgrow = Priority.ALWAYS
     hgrow = Priority.ALWAYS
-    content = Seq(
-      new Button {
-        text = "Open"
-        onMouseClicked = new EventHandler[MouseEvent] {
-          override def handle(event:MouseEvent) {
-            event.consume()
-            fchooser.showOpenMultipleDialog(parent.value.getScene.getWindow) match {
-              case null => Unit
-              case fs => {
-                musicRecItems.clear
-                musicRecItems.addAll(fs.map(f => new MusicRecordItem(f.getName, "mp3")))
-              }
-            }
-          }
-        }
-      },
-      musicRecTable
-    )
+    content = Seq(playerControlsLayout, musicRecTable)
   }
 
   stage = new PrimaryStage {
