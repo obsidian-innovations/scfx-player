59242384bd11d9141ec3eb53bc58c58e7c855015
Andrei Oprisan <aoprisan@gmail.com>
2013-09-25 10:06:05 +0300

    using metadata

--- src/main/scala/org/scfxplayer/ScFxMediaTagTest.scala
+++ src/main/scala/org/scfxplayer/ScFxMediaTagTest.scala
@@ -1,24 +1,135 @@
 package org.scfxplayer
 
-import scalafx.application.JFXApp
-import scalafx.scene.media.{MediaPlayer, Media}
 import java.io.File
+import javafx.event.EventHandler
+import javafx.scene.input.MouseEvent
+import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.scene.Scene
+import scalafx.scene.media._
+import scalafx.stage.FileChooser
+import scalafx.beans.property.StringProperty
+import scalafx.collections.ObservableBuffer
+import scalafx.scene.control.TableColumn._
+import scalafx.scene.control.{TableColumn, TableView, Button}
+import scalafx.scene.layout.{HBox, Priority, VBox}
+import scalafx.geometry.Pos
+import scala.collection.JavaConversions._
+import scala.collection.mutable.{Map => MMap, HashMap => MHashMap, SynchronizedMap}
 
+case class TagMetaData(artist:String,track:String,album:String)
+object TagMetaData {
+  val ARTIST = "artist"
+  val TRACK = "title"
+  val ALBUM = "album"
+}
 
-object ScFxMediaTagTest extends JFXApp {
+object MetaData {
+  def apply(file: File)(f: TagMetaData => Unit):Unit = {
+    //val file = new File(fname)
 
-  val fname = "test-data/test.mp3"
-  val file = new File(fname)
+    val m = new Media(file.toURI.toString)
+    val mediaPlayer = new MediaPlayer(m)
 
-  val m = new Media(file.toURI.toString)
-  val mediaPlayer = new MediaPlayer(m)
+    mediaPlayer.onReady = {
+      val mt = m.metadata
+      println(mt)
+      val tag = TagMetaData(
+        artist = mt.get(TagMetaData.ARTIST).asInstanceOf[String],
+        track = mt.get(TagMetaData.TRACK).asInstanceOf[String],
+        album = mt.get(TagMetaData.ALBUM).asInstanceOf[String]
+      )
+      f(tag)
+    }
+  }
+}
 
-  mediaPlayer.onReady = {
-    println(mediaPlayer.status)
-    println(m.source)
-    println(m.tracks)
-    println(m.getDuration)
-    println(m.getMetadata)
+class TMusicRecordItem( fileName_ : String,
+                        artist_ : String,
+                        track_ : String,
+                        album_ : String) {
+  val fileName = new StringProperty(this, "fileName", fileName_)
+  val artist = new StringProperty(this, "artist", artist_)
+  val track = new StringProperty(this, "track", track_)
+  val album = new StringProperty(this, "album", album_)
+}
+
+object TMain extends JFXApp {
+
+  val fchooser:FileChooser = new FileChooser()
+  val f = new FileChooser.ExtensionFilter("MP3 (MPEG-1 or MPEG-2 Audio Layer III)", Seq("*.mp3", "*.MP3"))
+  fchooser.getExtensionFilters.addAll(f)
+
+  val musicRecItems = ObservableBuffer[TMusicRecordItem]()
+  val musicRecTable = new TableView[TMusicRecordItem](musicRecItems) {
+    vgrow = Priority.ALWAYS
+    hgrow = Priority.ALWAYS
+    columns ++= List(
+      new TableColumn[TMusicRecordItem, String] {
+        text = "Artist"
+        cellValueFactory = {_.value.artist}
+        prefWidth = 180
+      },
+      new TableColumn[TMusicRecordItem, String]() {
+        text = "Track"
+        cellValueFactory = {_.value.track}
+        prefWidth = 180
+      }
+    )
   }
 
+  val deleteFilesBtn = new Button {
+    text = "Delete"
+    prefHeight = 40
+    vgrow = Priority.NEVER
+  }
+
+  val openFilesBtn = new Button {
+    text = "Open"
+    vgrow = Priority.ALWAYS // Doesn't work?!?
+    managed = true
+    prefHeight = 40 // vgrow policy didn't work, set it directly for now
+    onMouseClicked = new EventHandler[MouseEvent] {
+      override def handle(event:MouseEvent) {
+        event.consume()
+        fchooser.showOpenMultipleDialog(parent.value.getScene.getWindow) match {
+          case null => Unit
+          case fs => {
+            musicRecItems.clear
+            val tmp = new MHashMap[Int,TMusicRecordItem] with SynchronizedMap[Int,TMusicRecordItem] {}
+            fs.map{ f => MetaData(f){ md =>
+              musicRecItems += new TMusicRecordItem(f.getName, md.artist,md.track,md.album)
+            }}
+          }
+        }
+      }
+    }
+  }
+
+  val playerControlsLayout = new HBox {
+    hgrow = Priority.ALWAYS
+    alignment = Pos.CENTER_LEFT
+    minHeight = 40
+    maxHeight = 40
+    prefHeight = 40
+    fillHeight = true // Doesn't work?!?
+    content = Seq(openFilesBtn, deleteFilesBtn)
+  }
+
+  val mainLayout = new VBox {
+    vgrow = Priority.ALWAYS
+    hgrow = Priority.ALWAYS
+    content = Seq(playerControlsLayout, musicRecTable)
+  }
+
+  stage = new PrimaryStage {
+    title = "Demo ScalaFX Player"
+    width = 640
+    height = 480
+    scene = new Scene {
+      width onChange {mainLayout.setPrefWidth(scene.value.getWidth);}
+      height onChange {mainLayout.setPrefHeight(scene.value.getHeight);}
+      content = mainLayout
+    }
+  }
 }
