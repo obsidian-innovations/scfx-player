e9b24ee6fb5e1d1471042a3f69a5273f9766c567
ssemakin <ssemakin@gmail.com>
2013-09-25 19:02:04 +0300

    Added playlist column settings menu to hide/show columns.

--- src/main/scala/org/scfxplayer/Main.scala
+++ src/main/scala/org/scfxplayer/Main.scala
@@ -6,30 +6,32 @@ import javafx.scene.input.MouseEvent
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.scene.Scene
-import scalafx.stage.FileChooser
+import scalafx.stage.{FileChooser, WindowEvent}
 import scalafx.collections.ObservableBuffer
 import scalafx.scene.control.TableColumn._
-import scalafx.scene.control.{SelectionMode, TableColumn, TableView, Button}
+import scalafx.scene.control._
 import scalafx.scene.layout.{Priority, HBox, VBox}
-import scalafx.geometry.Pos
-import scalafx.scene.media.{Media, MediaPlayer, MediaView, MediaErrorEvent}
+import scalafx.geometry.{Side, Pos}
+import scalafx.Includes._
+import javafx.scene.control.CheckMenuItem
+import scalafx.event.{Event, ActionEvent}
+
+//import scalafx.scene.media.{Media, MediaPlayer, MediaView, MediaErrorEvent}
 
 object Main extends JFXApp {
 
-  import scalafx.Includes._
-
-  val testMp3 = new File("test-data/test.mp3")
-  val media = new Media(testMp3.toURI.toURL.toExternalForm)
-  val mplayer = new MediaPlayer(media)
-  val playerView = new MediaView {
-    mediaPlayer = mplayer
-    fitHeight = 40
-    fitWidth = 300
-    hgrow = Priority.ALWAYS
-    vgrow = Priority.ALWAYS
-    smooth = true
-    onError = (event: MediaErrorEvent) => println("Media view error: " + event)
-  }
+//  val testMp3 = new File("test-data/test.mp3")
+//  val media = new Media(testMp3.toURI.toURL.toExternalForm)
+//  val mplayer = new MediaPlayer(media)
+//  val playerView = new MediaView {
+//    mediaPlayer = mplayer
+//    fitHeight = 40
+//    fitWidth = 300
+//    hgrow = Priority.ALWAYS
+//    vgrow = Priority.ALWAYS
+//    smooth = true
+//    onError = (event: MediaErrorEvent) => println("Media view error: " + event)
+//  }
 
   val fchooser:FileChooser = new FileChooser()
   val f = new FileChooser.ExtensionFilter("MP3 (MPEG-1 or MPEG-2 Audio Layer III)", Seq("*.mp3", "*.MP3"))
@@ -38,42 +40,74 @@ object Main extends JFXApp {
   fchooser.setInitialDirectory(new File(System.getProperty("user.home")))
 
   val musicRecItems = ObservableBuffer[MusicRecordItem]()
+  val durationColumn = new TableColumn[MusicRecordItem, String]() {
+    text = "Duration"
+    prefWidth = 80
+    minWidth = 50
+    cellValueFactory = {_.value.duration}
+  }
+  val trackColumn = new TableColumn[MusicRecordItem, String] {
+    text = "Track"
+    prefWidth = 300
+    minWidth = 50
+    cellValueFactory = {_.value.trackNameMade}
+  }
+  val albumColumn = new TableColumn[MusicRecordItem, String]() {
+    text = "Album"
+    prefWidth = 120
+    minWidth = 50
+    cellValueFactory = {_.value.album}
+  }
+  val artistColumn = new TableColumn[MusicRecordItem, String]() {
+    text = "Artist"
+    prefWidth = 120
+    minWidth = 50
+    cellValueFactory = {_.value.artist}
+  }
   val musicRecTable = new TableView[MusicRecordItem](musicRecItems) {
     vgrow = Priority.ALWAYS
     hgrow = Priority.ALWAYS
-    columns ++= List(
-      new TableColumn[MusicRecordItem, String]() {
-        text = "Duration"
-        prefWidth = 80
-        minWidth = 50
-        cellValueFactory = {_.value.duration}
-      },
-      new TableColumn[MusicRecordItem, String] {
-        text = "Track"
-        prefWidth = 300
-        minWidth = 50
-        cellValueFactory = {_.value.trackNameMade}
-      },
-      new TableColumn[MusicRecordItem, String]() {
-        text = "Album"
-        prefWidth = 120
-        minWidth = 50
-        cellValueFactory = {_.value.album}
-      },
-      new TableColumn[MusicRecordItem, String]() {
-        text = "Artist"
-        prefWidth = 120
-        minWidth = 50
-        cellValueFactory = {_.value.artist}
-      }
-    )
+    columns ++= List(durationColumn, trackColumn, albumColumn, artistColumn)
   }
   musicRecTable.selectionModel.value.setSelectionMode(SelectionMode.MULTIPLE)
+
+  def initPlaylistMenuItem(i:CheckMenuItem, c:TableColumn[_,_]):Unit = {
+    i.setSelected(true)
+    i.setOnAction(
+      new EventHandler[javafx.event.ActionEvent] {
+        override def handle(event:javafx.event.ActionEvent) {
+          event.consume()
+          c.setVisible(i.isSelected)
+        }
+      })
+  }
+  val durationMenuItem = new CheckMenuItem("Duration")
+  initPlaylistMenuItem(durationMenuItem, durationColumn)
+  val trackMenuItem = new CheckMenuItem("Track")
+  initPlaylistMenuItem(trackMenuItem, trackColumn)
+  val albumMenuItem = new CheckMenuItem("Album")
+  initPlaylistMenuItem(albumMenuItem, albumColumn)
+  val artistMenuItem = new CheckMenuItem("Artist")
+  initPlaylistMenuItem(artistMenuItem, artistColumn)
+  val playListSettingsMnu:ContextMenu = new ContextMenu {
+    style = "-fx-background-radius: 10 0 10 10; -fx-border-color: white; -fx-border-radius: 10 0 10 10;"
+    items ++= List(durationMenuItem, trackMenuItem, albumMenuItem, artistMenuItem)
+  }
+  val playlistSettingsBtn:Button = new Button {
+    text = "..."
+    onMouseClicked = new EventHandler[MouseEvent] {
+      override def handle(event:MouseEvent) {
+        event.consume()
+        playListSettingsMnu.show(parent.value.getScene.content.head, Side.LEFT, event.getSceneX, event.getSceneY)
+      }
+    }
+  }
 
   val deleteFilesBtn = new Button {
     text = "Delete"
     prefHeight = 40 // vgrow policy didn't work, set it directly for now
     vgrow = Priority.ALWAYS // Doesn't work?!?
+    hgrow = Priority.ALWAYS
     onMouseClicked = new EventHandler[MouseEvent] {
       override def handle(event:MouseEvent) {
         event.consume()
@@ -108,7 +142,7 @@ object Main extends JFXApp {
     maxHeight = 40
     prefHeight = 40
     fillHeight = true // Doesn't work?!?
-    content = Seq(openFilesBtn, deleteFilesBtn, playerView)
+    content ++= List(openFilesBtn, deleteFilesBtn, playlistSettingsBtn)
   }
 
   val mainLayout = new VBox {
@@ -125,6 +159,7 @@ object Main extends JFXApp {
       width onChange {mainLayout.setPrefWidth(scene.value.getWidth);}
       height onChange {mainLayout.setPrefHeight(scene.value.getHeight);}
       content = mainLayout
+      onCloseRequest = (event:WindowEvent) => {}
     }
   }
 }
