97f8b84f55c96256d764319cbeb7bcfe5fdf3bf4
Andrei Oprisan <aoprisan@gmail.com>
2013-09-25 17:54:44 +0300

    initial playlist serialization impl

--- build.sbt
+++ build.sbt
@@ -8,10 +8,18 @@ scalaVersion := "2.10.2"
 
 assemblySettings
 
+resolvers ++= Seq(
+    "Mandubian repository snapshots" at "https://github.com/mandubian/mandubian-mvn/raw/master/snapshots/",
+    "Mandubian repository releases" at "https://github.com/mandubian/mandubian-mvn/raw/master/releases/"
+    )
+
+
+
 libraryDependencies ++= Seq(
                            "org.specs2" %% "specs2" % "2.2.2" % "test",
                             "joda-time" % "joda-time" % "2.3",
                             "org.joda" % "joda-convert" % "1.4",
+                            "play" %% "play-json" % "2.2-SNAPSHOT",
 //to be removed
 //                            "org" % "jaudiotagger" % "2.0.3",
 // current stuff
--- src/main/scala/org/scfxplayer/MusicRecordItem.scala
+++ src/main/scala/org/scfxplayer/MusicRecordItem.scala
@@ -4,11 +4,13 @@ import scalafx.beans.property.StringProperty
 import org.joda.time.Duration
 import org.joda.time.format.PeriodFormatterBuilder
 
-class MusicRecordItem(artist_ : Option[String],
-                      album_ : Option[String],
-                      track_ : Option[String],
-                      duration_ : Duration,
-                      fileName_ : String) {
+import play.api.libs.json._
+
+class MusicRecordItem(val artist_ : Option[String],
+                      val album_ : Option[String],
+                      val track_ : Option[String],
+                      val duration_ : Duration,
+                      val fileName_ : String) {
   val artist = new StringProperty(this, "artist", artist_.getOrElse(""))
   val album = new StringProperty(this, "album", album_.getOrElse(""))
   val track = new StringProperty(this, "track", track_.getOrElse(""))
@@ -33,4 +35,50 @@ class MusicRecordItem(artist_ : Option[String],
 
   val duration = new StringProperty(this, "duration", durationToString(duration_))
 
+}
+
+object MusicRecordItem {
+
+  val jsArtist = "artist"
+  val jsTrack = "track"
+  val jsAlbum = "album"
+  val jsDuration = "duration"
+  val jsFileName = "filename"
+
+  implicit val durationFormat = new Format[Duration] {
+    def reads(json: JsValue): JsResult[Duration] = json.validate[Long].map(l => new Duration(l))
+
+    def writes(o: Duration): JsValue = Json.toJson(o.getMillis)
+  }
+
+
+  implicit val format = new Format[MusicRecordItem] {
+    import Json._
+
+    def reads(json: JsValue): JsResult[MusicRecordItem] = for {
+      artist <- (json \ jsArtist).validate[Option[String]]
+      track <- (json \ jsTrack).validate[Option[String]]
+      album <- (json \ jsAlbum).validate[Option[String]]
+      duration <- (json \ jsDuration).validate[Duration]
+      filename <- (json \ jsFileName).validate[String]
+    } yield new MusicRecordItem(artist,track,album,duration,filename)
+
+    def writes(o: MusicRecordItem): JsValue = obj(
+      jsArtist -> o.artist_, jsTrack -> o.track_,
+      jsAlbum -> o.album_, jsDuration -> o.duration_,
+    jsFileName -> o.fileName_
+    )
+  }
+
+  def save(filename:String, items:List[MusicRecordItem]):Either[String,Unit] = {
+    import Writes._
+    val playlist = Json.stringify(Json.toJson(items))
+    val output = new java.io.PrintWriter(new java.io.File(filename))
+    try {
+      output.write(playlist)
+    } finally {
+      output.close()
+    }
+    Right(())
+  }
 }
