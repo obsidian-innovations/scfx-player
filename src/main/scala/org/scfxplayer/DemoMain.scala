b5359992c2494deeb1a633e6948b309338929ad3
ssemakin <ssemakin@gmail.com>
2013-09-26 18:18:29 +0300

    Implemented a simple test player with a play/pause button and position slider. Not bound to the main GUI.

--- src/main/scala/org/scfxplayer/DemoMain.scala
+++ src/main/scala/org/scfxplayer/DemoMain.scala
@@ -1,15 +1,19 @@
 package org.scfxplayer
 
+import java.io.File
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.scene.Scene
-import scalafx.scene.layout.HBox
-import scalafx.scene.paint.Color._
-import scalafx.scene.effect._
-import scalafx.scene.paint.{ Stops, LinearGradient }
-import scalafx.scene.text.Text
+import scalafx.scene.layout.{VBox, Priority, HBox}
 import javafx.event.EventHandler
 import javafx.stage.WindowEvent
+import scalafx.scene.control.{Slider, Button}
+import scalafx.scene.media.{Media, MediaPlayer, MediaView, MediaErrorEvent}
+import scalafx.scene.input.MouseEvent
+import scalafx.util.Duration
+import scalafx.geometry.Pos
+import scala.util.Try
+import scalafx.event
 
 // *************** Some reading material **************** //
 //http://search.maven.org/#search%7Cga%7C1%7Cscalafx
@@ -20,63 +24,97 @@ import javafx.stage.WindowEvent
 
 object DemoMain extends JFXApp {
 
-  val scalaText = new Text {
-    text = "Scala"
-    style = "-fx-font-size: 100pt"
-    fill = new LinearGradient(
-      endX = 0,
-      stops = Stops(PALEGREEN, SEAGREEN))
-  }
+  import scalafx.Includes._
 
-  val fxText = new Text {
-    text = "FX"
-    style = "-fx-font-size: 100pt"
-    fill = new LinearGradient(
-      endX = 0,
-      stops = Stops(CYAN, DODGERBLUE))
-    effect = new DropShadow {
-      color = DODGERBLUE
-      radius = 25
-      spread = 0.25
-    }
-  }
+  val files = List(new File("test-data/test.mp3"), new File("test-data/test.mp3"), new File("test-data/test.mp3"))
 
-  val playerText = new Text {
-    text = " Player"
-    style = "-fx-font-size: 100pt"
-    fill = new LinearGradient(
-      endX = 0,
-      stops = Stops(PALEGREEN, SEAGREEN))
-  }
-
-  val textSeq = Seq(scalaText, fxText, playerText)
-
-  val textBox = new HBox {
-    content = textSeq
-    effect = new Reflection {
-      topOffset = -50
-      bottomOpacity = 0.75
-      input = new Lighting {
-        light = new Light.Distant {
-          elevation = 60
-        }
+  def playPlaylist(pl:List[File]) {
+    pl.headOption.map { f => if(f.exists) {
+      val media = new Media(f.toURI.toURL.toExternalForm)
+      val mplayer = new MediaPlayer(media)
+      mplayer.onReady = onPlayerReady(mplayer)
+      mplayer.onEndOfMedia = {
       }
+    }}
+  }
+
+  val timePosSlider = new Slider {
+    style = "-fx-padding: 0 10 0 10;"
+    hgrow = Priority.ALWAYS
+  }
+
+  val playBtn:Button = new Button {
+    text = "Play"
+    prefWidth = 100
+    onMouseClicked = (event:MouseEvent) => {
+      event.consume()
+      playPlaylist(files)
     }
+  }
+
+  def isPlaying(p:MediaPlayer) = p.status.value.toString == MediaPlayer.Status.PLAYING.toString()
+
+  def onPlayClicked(player:MediaPlayer)(event:MouseEvent) = {
+    event.consume()
+    if(isPlaying(player)) player.pause()
+    else player.play()
+  }
+
+  def onPlayerReady(mplayer:MediaPlayer) = {
+    mplayer.onPlaying = {playBtn.text = "Pause"}
+    mplayer.onPaused = {playBtn.text = "Play"}
+    playBtn.onMouseClicked = onPlayClicked(mplayer)_
+    timePosSlider.min = 0.0
+    timePosSlider.max = mplayer.media.duration.value.toSeconds
+    timePosSlider.value = 0.0
+    timePosSlider.value onChange {
+      if(!timePosSlider.valueChanging.value || !isPlaying(mplayer))
+        mplayer.seek(Duration(timePosSlider.value.value * 1000.0))
+    }
+    mplayer.currentTime onChange {
+        timePosSlider.valueChanging = true
+        Try(timePosSlider.value = mplayer.currentTime.value.toSeconds)
+        timePosSlider.valueChanging = false
+    }
+    mplayer.play()
+  }
+
+//  val playerView = new MediaView {
+//    mediaPlayer = mplayer
+//    fitHeight = 40
+//    fitWidth = 300
+//    hgrow = Priority.ALWAYS
+//    vgrow = Priority.ALWAYS
+//    smooth = true
+//    onError = (event: MediaErrorEvent) => println("Media view error: " + event)
+//  }
+
+  val playerControlsLayout = new HBox {
+    prefHeight = 40
+    minHeight = 40
+    maxHeight = 40
+    alignment = Pos.CENTER
+    hgrow = Priority.ALWAYS
+    vgrow = Priority.ALWAYS
+    content = Seq(playBtn, timePosSlider)
+  }
+
+  val mainLayout = new VBox {
+    content = Seq(playerControlsLayout)
   }
 
   stage = new PrimaryStage {
     title = "ScalaFX Demo Player"
     width = 650
-    height = 450
+    height = 100
     scene = new Scene {
-      fill = BLACK
-      content = textBox
+      width onChange {mainLayout.setPrefWidth(scene.value.getWidth)}
+      height onChange {mainLayout.setPrefHeight(scene.value.getHeight)}
+      content = mainLayout
     }
     onShown = new EventHandler[WindowEvent] {
       override def handle(event:WindowEvent) {
         event.consume()
-        stage.setWidth(textSeq.foldLeft(0.0){(r, c) => c.boundsInLocal.value.getWidth + r})
-        stage.setHeight(textBox.boundsInLocal.value.getHeight)
       }
     }
   }
